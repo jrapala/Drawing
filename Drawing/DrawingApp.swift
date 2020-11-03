f48640d145b6eacf9447d93a487cf8d0d3ad6892
Juliette Rapala <jrapala@gmail.com>
2020-11-03 06:49:17 -0600

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Juliette Rapala on 11/3/20.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

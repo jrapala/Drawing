fb9998980140eefba529873c402be14ee8a575bb
Juliette Rapala <jrapala@gmail.com>
2020-11-10 18:17:52 -0600

    feat: add triangle drawing

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -8,11 +8,60 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var arrowThickness: CGFloat = 50
+    @State private var colorCycle = 0.0
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(spacing: 0) {
+            ArrowTop()
+            ArrowBottom(thickness: arrowThickness)
+        }
+        .frame(width: 200, height: 500)
+        .onTapGesture {
+            withAnimation {
+                self.arrowThickness = CGFloat.random(in: 10...100)
+            }
+        }
+        .foregroundColor(.purple)
     }
 }
+
+struct ArrowTop: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        path.move(to: CGPoint(x:rect.midX, y:rect.midY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.midX, y: rect.midY))
+        
+
+        return path
+    }
+}
+
+struct ArrowBottom: Shape {
+    var thickness: CGFloat
+    
+    var animatableData: CGFloat {
+        get { thickness }
+        set { self.thickness = newValue }
+    }
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        path.move(to: CGPoint(x:rect.midX - thickness / 2, y:rect.minY))
+        path.addLine(to: CGPoint(x: rect.midX - thickness / 2, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.midX - thickness / 2 + thickness, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.midX - thickness / 2 + thickness, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.midX - thickness / 2, y: rect.minY))
+        
+        return path
+    }
+}
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
